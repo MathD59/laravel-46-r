e3cc8ac9566b2a971198440a5b49bc0e1f1989b7
MathD59 <mathieu1duchateau@gmail.com>
2023-01-06 22:00:49 +0100

    god 2

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,45 +1,127 @@
 <?php
 
+
+
 namespace App\Models;
 
+
+
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Foundation\Auth\User as Authenticatable;
+
 use Illuminate\Notifications\Notifiable;
+
 use Laravel\Passport\HasApiTokens;
 
+
+
 class User extends Authenticatable
+
 {
+
     use HasApiTokens, HasFactory, Notifiable;
 
+
+
     /**
+
      * The attributes that are mass assignable.
+
      *
+
      * @var array<int, string>
+
      */
+
     protected $fillable = [
+
         'name',
+
         'email',
+
         'password',
+
         'google_id',
-    ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
         'password',
-        'remember_token',
+
+        'widget1',
+
+        'params1',
+
+        'timer1',
+
+        'widget2',
+
+        'params2',
+
+        'timer2',
+
+        'widget3',
+
+        'params3',
+
+        'timer3',
+
+        'widget4',
+
+        'params4',
+
+        'timer4',
+
+        'widget5',
+
+        'params5',
+
+        'timer5',
+
+        'widget6',
+
+        'params6',
+
+        'timer6',
+
     ];
 
+
+
     /**
-     * The attributes that should be cast.
+
+     * The attributes that should be hidden for serialization.
+
      *
-     * @var array<string, string>
+
+     * @var array<int, string>
+
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
+
+    protected $hidden = [
+
+       
+
+        'remember_token',
+
     ];
+
+
+
+    /**
+
+     * The attributes that should be cast.
+
+     *
+
+     * @var array<string, string>
+
+     */
+
+    protected $casts = [
+
+        'email_verified_at' => 'datetime',
+
+    ];
+
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,37 +1,111 @@
 <?php
 
+
+
 use Illuminate\Database\Migrations\Migration;
+
 use Illuminate\Database\Schema\Blueprint;
+
 use Illuminate\Support\Facades\Schema;
 
+
+
 return new class extends Migration
+
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password')->nullable();
-            $table->string('google_id')->unique()->nullable();
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
 
     /**
-     * Reverse the migrations.
+
+     * Run the migrations.
+
      *
+
      * @return void
+
      */
-    public function down()
+
+    public function up()
+
     {
-        Schema::dropIfExists('users');
+
+        Schema::create('users', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->string('name');
+
+            $table->string('email')->unique();
+
+            $table->timestamp('email_verified_at')->nullable();
+
+            $table->string('password')->nullable();
+
+            $table->string('google_id')->unique()->nullable();;
+
+            $table->rememberToken();
+
+            $table->timestamps();
+
+            $table->string('widget1')->nullable();;
+
+            $table->string('params1')->nullable();;
+
+            $table->string('timer1')->nullable();;
+
+            $table->string('widget2')->nullable();;
+
+            $table->string('params2')->nullable();;
+
+            $table->string('timer2')->nullable();;
+
+            $table->string('widget3')->nullable();;
+
+            $table->string('params3')->nullable();;
+
+            $table->string('timer3')->nullable();;
+
+            $table->string('widget4')->nullable();;
+
+            $table->string('params4')->nullable();;
+
+            $table->string('timer4')->nullable();;
+
+            $table->string('widget5')->nullable();;
+
+            $table->string('params5')->nullable();;
+
+            $table->string('timer5')->nullable();;
+
+            $table->string('widget6')->nullable();;
+
+            $table->string('params6')->nullable();;
+
+            $table->string('timer6')->nullable();;
+
+
+
+        });
+
     }
+
+
+
+    /**
+
+     * Reverse the migrations.
+
+     *
+
+     * @return void
+
+     */
+
+    public function down()
+
+    {
+
+        Schema::dropIfExists('users');
+
+    }
+
 };
