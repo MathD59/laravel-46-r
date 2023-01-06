1bdac8d1f142d9bfbeab8a76b1fc811a1e8fc673
MathD59 <mathieu1duchateau@gmail.com>
2023-01-06 23:17:08 +0100

    policerendezvous

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -21,23 +21,28 @@ class GoogleAuthController extends Controller
     public function callbackGoogle()
     {
         try {
+            echo ("toto1");
             $google_user = Socialite::driver('google')->user();
+            echo ("toto2");
             $user = User::where('google_id', $google_user->getId())->first();
-            // dd($user);
+            echo ("toto3");
             if (!$user) {
-                echo ("toto1");
+                echo ("toto4");
                 $new_user = User::create([
                     'name' => $google_user->getName(),
                     'email' => $google_user->getEmail(),
                     'google_id' => $google_user->getId(),
                 ]);
+                echo ("toto5");
                 $token = $new_user->createToken('My Token')->accessToken;
                 return response()->json(['token' => $token], 200);
             } else {
+                echo ("toto6");
                 $token = $user->createToken('My Token')->accessToken;
                 return response()->json(['token' => $token], 200);
             }
         } catch (Exception $e) {
+            echo ("toto7");
             dd($e->getMessage());
         }
     }
