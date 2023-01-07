bb4cd490f5952eefa0dafa476fe21d18dcf8dfe9
MathD59 <mathieu1duchateau@gmail.com>
2023-01-07 10:46:55 +0100

    i believe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,9 +25,10 @@
                     </div>
 
                     <?php 
-                    ini_set('curl.cainfo', '/cacert.pem');
+                        ini_set('curl.cainfo', './cacert.pem');
                     
-                    phpinfo() ?> 
+                        phpinfo();
+                    ?> 
 
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
