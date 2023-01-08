9d718322126931db959e9bb856bcbf66d8adc5cd
MathD59 <mathieu1duchateau@gmail.com>
2023-01-08 21:24:07 +0100

    2h30

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,5 +32,34 @@ class UserController extends Controller
         }
         // return $user;
     }
+
+    
+    public function usercfg (Request $request, $id)
+    {
+            $newcfg=User::find($id)->update(
+            [
+                'name'=>$request->name,
+                'email'=>$request->email,
+                'password'=>$request->password,
+                'widget1'=>$request->widget1,
+                'params1'=>$request->params1,
+                'timer1'=>$request->timer1,
+                'widget2'=>$request->widget1,
+                'params2'=>$request->params1,
+                'timer2'=>$request->timer1,
+                'widget3'=>$request->widget1,
+                'params3'=>$request->params1,
+                'timer3'=>$request->timer1,
+                'widget4'=>$request->widget1,
+                'params4'=>$request->params1,
+                'timer4'=>$request->timer1,
+                'widget5'=>$request->widget1,
+                'params5'=>$request->params1,
+                'timer5'=>$request->timer1,
+                'widget6'=>$request->widget1,
+                'params6'=>$request->params1,
+                'timer6'=>$request->timer1 ]);
+            return $newcfg;    
+    }
 }
 
