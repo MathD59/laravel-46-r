24da9663a25f72e9de66ea6f148390d6f8cccb85
MathD59 <mathieu1duchateau@gmail.com>
2023-01-08 22:35:50 +0100

    retour niveau 0

--- routes/api.php
+++ routes/api.php
@@ -17,11 +17,8 @@ Route::get('/test', function (Request $request){
     return "Authenticated";
 });
 
-Route::middleware('auth:api')->group(function(){
-    Route::get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-    });
-    Route::put('/mod', [UserController::class, 'usercfg']);
 });
 
 // ROUTE SUPPLEMENTAIRE AVEC UN PREFIX ET UN GROUP POUR DONNER INFORMATION SUR DES VERSIONS. 
@@ -30,4 +27,11 @@ Route::middleware('auth:api')->group(function(){
 //     Route::get('/user', function (Request $request){
 //         return $request->user();
 //     });
+// });
+
+// Route::middleware('auth:api')->group(function(){
+//     Route::get('/user', function (Request $request) {
+//     return $request->user();
+//     });
+//     Route::put('/mod', [UserController::class, 'usercfg']);
 // });
